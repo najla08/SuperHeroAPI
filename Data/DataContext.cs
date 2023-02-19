39879b934bbbac0d1fcef208e6c6ab3ac4b812a9
najla08 <najlalataleb@gmail.com>
2023-02-19 03:01:40 +0300

    push

---
+++ Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+	public class DataContext: DbContext
+	{
+		public DataContext(DbContextOptions<DataContext>options): base(options)
+		{
+
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=superherodb;User ID='sa';Password='Najla009';Encrypt=false;MultipleActiveResultSets=true");
+		}
+
+		public DbSet<SuperHero> SuperHeroes { get; set; }
+	}
+}
+
---
+++ Services/SuperHeroService/ISuperHeroService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SuperHeroAPI.Services.SuperHeroService
+{
+	public interface ISuperHeroService
+	{
+        Task<List<SuperHero>> GetAllHeroes();
+        Task<SuperHero>? GetSingleHero(int id);
+        Task<List<SuperHero>> AddHero(SuperHero hero);
+        Task<List<SuperHero>?> UpdateHero(int id,SuperHero request);
+        Task<List<SuperHero>?> DeleteHero(int id);
+
+
+
+    }
+}
+
