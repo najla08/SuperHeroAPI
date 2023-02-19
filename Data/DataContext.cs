af36837b76837a1b246a0da2e13dffedd08fb338
najla08 <najlaaltaleb@gmail.com>
2023-02-19 03:06:59 +0300

    Update DataContext.cs

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -13,7 +13,7 @@ namespace SuperHeroAPI.Data
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			base.OnConfiguring(optionsBuilder);
-			optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=superherodb;User ID='sa';Password='Najla009';Encrypt=false;MultipleActiveResultSets=true");
+			optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=superherodb;User ID='sa';Password='';Encrypt=false;MultipleActiveResultSets=true");
 		}
 
 		public DbSet<SuperHero> SuperHeroes { get; set; }
